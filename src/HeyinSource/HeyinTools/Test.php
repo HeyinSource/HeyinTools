458014307380551c0487a12356e7c3003436163f
lt <751971678@qq.com>
2021-10-25 11:30:36 +0800

    init

---
+++ src/HeyinSource/HeyinTools/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lt
+ * Date: 2021/10/25
+ * Time: 11:19
+ */
+
+namespace HeyinSource\HeyinTools;
+
+
+class Test
+{
+    public static function Test()
+    {
+        echo 'hello Heyin';
+    }
+}
